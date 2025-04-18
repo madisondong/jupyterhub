86817b7e078b4f901fede2f10aed17844404e88b
madisondong <phamdinhdong02@gmail.com>
2025-04-18 10:02:12 -0400

    Update jupyterhub_config.py

--- jupyterhub_config.py
+++ jupyterhub_config.py
@@ -2,5 +2,8 @@ c = get_config()
 
 c.JupyterHub.bind_url = 'http://:8000'
 
+c.JupyterHub.authenticator_class = 'dummyauthenticator.DummyAuthenticator'
+c.DummyAuthenticator.password = "admin"
+
 c.Authenticator.admin_users = {'admin'}
 c.JupyterHub.admin_access = True
