55cc26febe7acd16f41afcf47b7b59bbc46b4583
madisondong <phamdinhdong02@gmail.com>
2025-04-18 17:34:53 -0400

    Update jupyterhub_config.py

--- jupyterhub_config.py
+++ jupyterhub_config.py
@@ -4,6 +4,10 @@ c.JupyterHub.bind_url = 'http://:8000'
 
 c.JupyterHub.authenticator_class = 'dummyauthenticator.DummyAuthenticator'
 c.DummyAuthenticator.password = "admin"  # shared password
+c.Authenticator.create_system_users = True
+
+c.Spawner.default_url = '/lab'
+c.Spawner.start_timeout = 60
 
 c.Authenticator.allowed_users = {'admin', 'alice'}
 c.Authenticator.admin_users = {'admin'}
